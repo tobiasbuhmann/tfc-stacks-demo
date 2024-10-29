7b30dff4425a21b3e99ea1fccb9e78de55f85791
ned1313 <ned.bellavance@gmail.com>
2024-10-29 13:45:55 -0400

    change module

--- database/main.tf
+++ database/main.tf
@@ -2,14 +2,21 @@ locals {
   name = "${var.prefix}-db-${var.suffix}"
 }
 
-module "sql_database" {
-  source              = "Azure/database/azurerm"
-  resource_group_name = local.name
-  location            = var.location
-  db_name             = local.name
-  sql_admin_username  = "mradministrator"
-  sql_password        = "P@ssw0rd12345!"
+resource "azurerm_resource_group" "main" {
+  name = local.name
+  location = var.location
+}
+
+module "avm-res-sql-server" {
+  source  = "Azure/avm-res-sql-server/azurerm"
+  version = "0.1.1"
+  
+  name = local.name
+  location = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  administrator_login = "mradministrator"
+  administrator_login_password = "P@ssw0rd12345!"
+  server_version = "12.0"
 
   tags = var.tags
-
 }
