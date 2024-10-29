bf8ea4b142120bb25871fd741c311c536954ebe8
ned1313 <ned.bellavance@gmail.com>
2024-10-29 13:31:42 -0400

    fix references

--- database/main.tf
+++ database/main.tf
@@ -4,8 +4,8 @@ locals {
 
 module "sql_database" {
   source              = "Azure/database/azurerm"
-  resource_group_name = azurerm_resource_group.main.name
-  location            = azurerm_resource_group.main.location
+  resource_group_name = local.name
+  location            = var.location
   db_name             = local.name
   sql_admin_username  = "mradministrator"
   sql_password        = "P@ssw0rd12345!"
